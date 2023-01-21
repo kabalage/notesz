0080f146f0d93a5c1489ef2ee248eb6a6b54fa80
Balázs Kaufmann <bkaufmann23@gmail.com>
2023-01-21 19:40:21 +0100

    Fix git blob hash generation

--- src/utils/gitBlobHash.ts
+++ src/utils/gitBlobHash.ts
@@ -1,6 +1,10 @@
 export default async function gitBlobHash(blob: string) {
-  const blobUint8 = new TextEncoder().encode(`blob ${blob.length}\0${blob}`);
-  const hashBuffer = await crypto.subtle.digest('SHA-1', blobUint8);
+  const blobEncoded = new TextEncoder().encode(blob);
+  const prefixEncoded = new TextEncoder().encode(`blob ${blobEncoded.length}\0`);
+  const objectEncoded = new Uint8Array(prefixEncoded.length + blobEncoded.length);
+  objectEncoded.set(prefixEncoded);
+  objectEncoded.set(blobEncoded, prefixEncoded.length);
+  const hashBuffer = await crypto.subtle.digest('SHA-1', objectEncoded);
   const hashArray = Array.from(new Uint8Array(hashBuffer));
   const hashHex = hashArray
     .map((bytes) => bytes.toString(16).padStart(2, '0'))
