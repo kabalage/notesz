413fd89fa82aa7af539758c69e598d26c124f6f1
Balázs Kaufmann <bkaufmann23@gmail.com>
2025-01-19 18:21:24 +0100

    Add workbox-window as direct dependency for pnpm, fix filterMap TS error

--- src/utils/mapUtils.ts
+++ src/utils/mapUtils.ts
@@ -18,14 +18,14 @@ export function keyByMap<K, V>(
 /**
  * It's like Array.prototype.filter, but works with Maps
  */
-export function filterMap<K, V, V2 extends V>(
+export function filterMap<K, V, T extends V = V>(
   map: Map<K, V>,
-  predicate: ((value: V, key: K) => value is V2) | ((value: V, key: K) => boolean)
+  predicate: ((value: V, key: K) => value is T) | ((value: V, key: K) => boolean)
 ) {
-  const result: Map<K, V2> = new Map();
+  const result: Map<K, T> = new Map();
   for (const [key, value] of map) {
     if (predicate(value, key)) {
-      result.set(key, value);
+      result.set(key, value as T);
     }
   }
   return result;
