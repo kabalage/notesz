3bcbf90b46701d86f47fa09590dae8ea35a482e5
Balázs Kaufmann <bkaufmann23@gmail.com>
2025-07-05 16:37:30 +0200

    Improve url matching

--- src/utils/codemirror/hyperlink.ts
+++ src/utils/codemirror/hyperlink.ts
@@ -2,9 +2,10 @@
 // https://github.com/uiwjs/react-codemirror/blob/v4.23.14/extensions/hyper-link/src/index.ts
 //
 // Things changed:
-//    nicer svg icon
-//    fix underline style to be applied to the child
-//    always use MatchDecorator
+//   - nicer svg icon
+//   - fix underline style to be applied to the child
+//   - always use MatchDecorator
+//   - better regexp to match urls, the original did not match trailing slashes
 
 import {
   ViewPlugin,
@@ -19,8 +20,8 @@ import type { Extension } from '@codemirror/state';
 
 // eslint-disable-next-line max-len
 const svgStr = '<svg xmlns="http://www.w3.org/2000/svg" width="16" height="16"  viewBox="0 0 20 20" fill="currentColor"><path d="M12.232 4.232a2.5 2.5 0 0 1 3.536 3.536l-1.225 1.224a.75.75 0 0 0 1.061 1.06l1.224-1.224a4 4 0 0 0-5.656-5.656l-3 3a4 4 0 0 0 .225 5.865.75.75 0 0 0 .977-1.138 2.5 2.5 0 0 1-.142-3.667l3-3Z" /><path d="M11.603 7.963a.75.75 0 0 0-.977 1.138 2.5 2.5 0 0 1 .142 3.667l-3 3a2.5 2.5 0 0 1-3.536-3.536l1.225-1.224a.75.75 0 0 0-1.061-1.06l-1.224 1.224a4 4 0 1 0 5.656 5.656l3-3a4 4 0 0 0-.225-5.865Z" /></svg>';
-
-const defaultRegexp = /\b((?:https?|ftp):\/\/[^\s/$.?#].[^\s]*)\b/gi;
+// eslint-disable-next-line max-len
+const defaultRegexp = /(http|ftp|https):\/\/([\w_-]+(?:(?:\.[\w_-]+)+))([\w.,@?^=%&:\/~+#-]*[\w@?^=%&\/~+#-])/gi;
 
 export interface HyperLinkState {
   at: number;
